e14f1ca2fdb18247bd61b6fa0b12978a5e9a6ae8
quangang.sheng <quangang.sheng@wz-inc.com>
2020-11-05 10:52:23 +0800

    support wget

--- install.sh
+++ install.sh
@@ -1,4 +1,35 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 V="2.8.0"
-curl --silent -L https://github.com/ervandew/eclim/releases/download/$V/eclim_$V.bin > eclim.bin && sh ./eclim.bin --yes --vimfiles=skip "$@"
+
+__download() {
+  url="$1"
+  dest="$2"
+  if command -v wget >& /dev/null; then
+    wget -c \
+      --no-check-certificate \
+      -O "$filename" \
+      "$url"
+  elif command -v curl >& /dev/null; then
+    curl --silent \
+      --output "$filename" \
+      -L "$url"
+  else
+    return 1
+  fi
+}
+
+__main() {
+  version=$1
+  url="https://github.com/ervandew/eclim/releases/download/$V/eclim_$V.bin"
+  shift
+  filename=eclim_$V.bin
+  if [ ! -f $filename ]; then
+    __download $url $filename
+  fi
+  if [ -f $filename ]; then
+    $SHELL $filename --yes --vimfiles=skip "$@"
+  fi
+}
+
+__main $V $@
